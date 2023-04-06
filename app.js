658ef52e84461cd34d2d39d7b6f97d0216f1916c
AJ Dorfman <ajdorfman91@gmail.com>
2023-04-06 09:09:40 -0700

    do exercise

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require("http");
+const PORT = 5002 
+// my 5000 does not work
+
+http.createServer((req,res)=>{
+    res.writeHead(200, { "content-type": "text/html"});
+    res.write("Hello world");
+    res.end();
+})
+
+
+.listen(PORT, ()=>{
+    console.log(`server is listening at local host ${PORT} port`)
+});
